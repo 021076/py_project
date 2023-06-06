913ed479c97361cf5bbc72260150d4ecb823cdf7
021076 <ro_k_sana@mail.ru>
2023-06-06 22:54:43 +0500

    add fanc sum

---
+++ fancs.py
@@ -0,0 +1,9 @@
+def sum_two_nums(num1, num2):
+    sum_nums = num1 + num2
+    return sum_nums
+
+num1 = int(input("Введите первое число:\n"))
+num2 = int(input("Введите второе число:\n"))
+
+print(sum_two_nums(num1, num2))
+
