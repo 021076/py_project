3bdd3e9e01d5d92645c5ca8b4d5a8c4a8d5f47d7
021076 <ro_k_sana@mail.ru>
2023-06-16 10:55:54 +0500

    call fanc sum from main.py, comment call from fancs.py

--- fancs.py
+++ fancs.py
@@ -1,9 +1,12 @@
 def sum_two_nums(num1, num2):
     sum_nums = num1 + num2
     return sum_nums
-
-num1 = int(input("Введите первое число:\n"))
-num2 = int(input("Введите второе число:\n"))
-
-print(sum_two_nums(num1, num2))
-
+def subtr_two_nums(num1, num2):
+    subtr_nums = num1 - num2
+    return subtr_nums
+#
+# num1 = int(input("Введите первое число:\n"))
+# num2 = int(input("Введите второе число:\n"))
+#
+# print(sum_two_nums(num1, num2))
+# print(subtr_two_nums(num1, num2))
---
+++ main.py
@@ -0,0 +1,6 @@
+from fancs import sum_two_nums
+
+num1 = int(input("Введите первое число:\n"))
+num2 = int(input("Введите второе число:\n"))
+
+print(sum_two_nums(num1, num2))
