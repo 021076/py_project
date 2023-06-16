d74fe629f7201de694a2fb9bc94265b88179eabd
021076 <ro_k_sana@mail.ru>
2023-06-16 11:04:27 +0500

    call fanc subtr from main.py, comment call sum

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fancs import sum_two_nums
+from fancs import subtr_two_nums
 
 num1 = int(input("Введите первое число:\n"))
 num2 = int(input("Введите второе число:\n"))
 
-print(sum_two_nums(num1, num2))
+# print(sum_two_nums(num1, num2))
+print(subtr_two_nums(num1, num2))
